57f9c3c25ad231635960651652187045245c4e3b
DevAmam-sudo <devaman.net@gmail.com>
2021-05-28 11:16:48 +0545

    view-engine setup

--- server.js
+++ server.js
@@ -2,32 +2,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const hbs = require('hbs');
 require('dotenv').config();
-const User = require('./model/user');
 const port = process.env.PORT || 5000;
 
 // file_path //
 const staticPath = path.join(__dirname , "/static/");
 
 // app middleware`s //
+app.set('view-engine', 'hbs');
 app.use(express.static(staticPath));
-app.use(bodyParser.json());
-
-// Database Connection //
-mongoose.connect(process.env.CONFIG , { useNewUrlParser: true , useUnifiedTopology: true } , () => {
-    console.log('Connected to DB');
-});
 
 // app routes //
 app.get('/' , (req , res) => {
-    res.sendFile(path.join(staticPath , 'register.html'));
+    res.render('register.hbs');
 });
 
-app.post('/api/register' , async (req , res) => {
-    console.log(req.body);
-    res.json({ status: 'ok' });
+app.post('/' , (req , res) => {
+    res.redirect('register.hbs');
 });
 
 // listening to port //
