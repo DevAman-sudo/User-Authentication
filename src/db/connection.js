35c13505de946bd529e6064790c839bc100cb711
DevAmam-sudo <devaman.net@gmail.com>
2021-05-29 17:00:51 +0545

    sucessfully added user to mongo database

--- src/db/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(`${porcess.env.CONFIG}/devdb` , {
-    
-}).then( () => {
-    console.log('connected to DB');
-});
