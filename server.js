85139ae57caf41883e710767cb73728f2d1d6103
DevAmam-sudo <devaman.net@gmail.com>
2021-06-10 20:13:57 +0545

    fetched data using async await function

--- server.js
+++ server.js
@@ -8,41 +8,49 @@ require('dotenv').config();
 const configDB = process.env.CONFIG;
 const port = process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname , '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.json());
-app.use( express.urlencoded({ extended: true }) );
-app.set('view-engine' , 'hbs');
+app.use(express.urlencoded({
+    extended: true
+}));
+app.set('view-engine', 'hbs');
 
 // database connection
-mongoose.connect(`mongodb://192.168.0.100/devaman` ,
-// mongoose.connect(`https://'${configDB}'/devaman` ,
-{ useNewUrlParser: true ,
-  useUnifiedTopology: true
-}).then( () => {
-    console.log('connected');
-}).catch( (error) => {
-    console.log(`database error found ${error}`);
-});
+mongoose.connect(`mongodb://192.168.0.100/devaman`,
+    // mongoose.connect(`https://'${configDB}'/devaman` ,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('connected');
+    }).catch((error) => {
+        console.log(`database error found ${error}`);
+    });
 
-app.get('/' , (req , res) => {
+app.get('/', (req, res) => {
     res.render('index.hbs');
 });
 
-app.post('/' , async (req , res) => {
-    try {
-        
-        const registerUser = new Register({
-            task: req.body.task
-        });
-        const registered = registerUser.save();
-        
-        res.status(201).send('sucess');
-        
-    } catch(error) {
-        res.status(404).send(error);
-    }
+app.post('/', async (req, res) => {
+
+    const createDocument = async () => {
+        try {
+            const registerUser = new Register({
+                task: req.body.task
+            });
+            const registered = await registerUser.save();
+            console.log(registered);
+
+            res.status(201).send('sucess');
+
+        } catch(error) {
+            res.status(404).send(error);
+        }
+    };
+    createDocument();
+
 });
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log('app is running on port 5000');
 });
